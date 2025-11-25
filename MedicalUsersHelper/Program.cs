e8579d17dd63011d02b6234a872803e97b2c2dd1
Gianfranco Diaz <gianfrancodiaz011@gmail.com>
2025-11-24 23:50:47 -0500

    Replace HTTP server with Photino's Load() API

--- MedicalUsersHelper/Program.cs
+++ MedicalUsersHelper/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Data;
-using Application.Core.Interfaces.DrugEnforcementAdministration;
+﻿using Application.Core.Interfaces.DrugEnforcementAdministration;
 using Application.Core.Interfaces.License;
 using Application.Core.Interfaces.NationalProviderIdentifier;
 using Application.Core.Service.DrugEnforcementAdministration;
@@ -17,6 +16,8 @@ using MedicalUsersHelper.MessageHandlers.Handlers;
 using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.DependencyInjection;
 using Photino.NET;
+using System.Data;
+using System.Text;
 
 namespace MedicalUsersHelper;
 
@@ -69,93 +70,25 @@ class Program
             {
                 var photinoWindow = (PhotinoWindow)sender!;
                 messageRouter.RouteMessage(photinoWindow, message);
-            });
-
-        // Start a simple HTTP server for cross-platform compatibility
-        var wwwrootPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot");
-        var serverUrl = "http://localhost:8765";
-
-        if (Directory.Exists(wwwrootPath))
-        {
-            // Start simple HTTP server in background
-            var serverTask = Task.Run(() => StartSimpleServer(wwwrootPath, 8765));
-
-            // Give server a moment to start
-            Thread.Sleep(500);
-
-            // Load from localhost
-            window.Load($"{serverUrl}/index.html");
-
-            // Wait for window to close
-            window.WaitForClose();
-        }
-        else
-        {
-            window.LoadRawString("<html><body><h1>Error: wwwroot folder not found</h1></body></html>");
-            window.WaitForClose();
-        }
-
-        // Simple HTTP server for serving static files
-        static void StartSimpleServer(string path, int port)
-        {
-            var listener = new System.Net.HttpListener();
-            listener.Prefixes.Add($"http://localhost:{port}/");
-
-            try
+            })
+            .RegisterCustomSchemeHandler("app", (object sender, string scheme, string url, out string contentType) =>
             {
-                listener.Start();
+                contentType = "text/plain";
+                var uri = new Uri(url);
+                var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot", uri.AbsolutePath.TrimStart('/'));
 
-                while (listener.IsListening)
+                if (File.Exists(filePath))
                 {
-                    var context = listener.GetContext();
-                    var request = context.Request;
-                    var response = context.Response;
-
-                    try
-                    {
-                        // Get requested file path
-                        var relativePath = request.Url?.LocalPath.TrimStart('/') ?? "index.html";
-                        if (string.IsNullOrEmpty(relativePath)) relativePath = "index.html";
-
-                        var filePath = Path.Combine(path, relativePath);
-
-                        if (File.Exists(filePath))
-                        {
-                            // Set content type
-                            response.ContentType = GetContentType(filePath);
-
-                            // Read and send file
-                            var buffer = File.ReadAllBytes(filePath);
-                            response.ContentLength64 = buffer.Length;
-                            response.OutputStream.Write(buffer, 0, buffer.Length);
-                            response.StatusCode = 200;
-                        }
-                        else
-                        {
-                            response.StatusCode = 404;
-                            var errorBytes = System.Text.Encoding.UTF8.GetBytes("404 - File Not Found");
-                            response.OutputStream.Write(errorBytes, 0, errorBytes.Length);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        response.StatusCode = 500;
-                    }
-                    finally
-                    {
-                        response.OutputStream.Close();
-                    }
+                    contentType = GetContentType(filePath);
+                    return new FileStream(filePath, FileMode.Open, FileAccess.Read);
                 }
-            }
-            catch (Exception ex)
-            {
 
-            }
-            finally
-            {
-                listener.Stop();
-            }
-        }
+                return new MemoryStream(Encoding.UTF8.GetBytes("404 - File Not Found"));
+            })
+            .Load("wwwroot/index.html");
+
+        // Run the app
+        window.WaitForClose();
 
         static string GetContentType(string filePath)
         {
@@ -175,8 +108,5 @@ class Program
                 _ => "application/octet-stream"
             };
         }
-
-        // Run the app
-        window.WaitForClose();
     }
 }
